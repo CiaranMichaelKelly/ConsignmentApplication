4c2b171aa33b3ee0d465c701eda70dc15d7b0e0e
Ciaran Kelly <92984593+CiaranMichaelKelly@users.noreply.github.com>
2021-12-07 13:06:45 +0100

    Connected UI to library

--- ConsignmentApplication/demo.cs
+++ ConsignmentApplication/demo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsignmentShopLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,16 +9,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ConsignmentApplication
 {
     public partial class demo : Form
     {
+        private Store store = new Store();
         public demo()
         {
             InitializeComponent();
         }
 
         private void addToCart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetUpData()
         {
 
         }
