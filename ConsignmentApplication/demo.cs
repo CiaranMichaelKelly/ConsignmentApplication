79453b90f626be225b4cd9f0717561ddf4e6a4ad
Ciaran Kelly <92984593+CiaranMichaelKelly@users.noreply.github.com>
2021-12-09 09:58:04 +0100

    Continued modifying the classes and started to create some items to populate the lists

--- ConsignmentApplication/demo.cs
+++ ConsignmentApplication/demo.cs
@@ -27,7 +27,33 @@ namespace ConsignmentApplication
 
         private void SetUpData()
         {
+            store.Vendors.Add(new Vendor { FirstName = "Joe", LastName = "Kelly" });
+            store.Vendors.Add(new Vendor { FirstName = "Henry", LastName = "Halls" });
+            store.Vendors.Add(new Vendor { FirstName = "Barry", LastName = "White" });
 
+            store.Items.Add(new Item
+            {
+                Title = "Moby Dick",
+                Description = "A book about a Whale",
+                Price = 5.50M,
+                Owner = store.Vendors[0]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "iPhone 13",
+                Description = "Apple iPhone 13",
+                Price = 1000M,
+                Owner = store.Vendors[2]
+            });
+
+            store.Items.Add(new Item
+            {
+                Title = "13 Laptop",
+                Description = "Dell Latitude 7458",
+                Price = 1500M,
+                Owner = store.Vendors[3]
+            });
         }
     }
 }
